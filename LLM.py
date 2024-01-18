88a352012aab3a8bc128ad5dc616f7e3440635f0
yestolife <zgjxncytl@gmail.com>
2024-01-18 20:05:00 +0800

    Update LLM.py

--- LLM.py
+++ LLM.py
@@ -11,13 +11,13 @@ class InternLM_LLM(LLM):
 
     def __init__(self, model_path :str):
         # model_path: InternLM 模型路径
-        # 从本地初始化模型
+        # 初始化模型
         super().__init__()
-        print("正在从本地加载模型...")
+        print("正在加载模型...")
         self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
         self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True).to(torch.bfloat16).cuda()
         self.model = self.model.eval()
-        print("完成本地模型的加载")
+        print("完成模型的加载")
 
     def _call(self, prompt : str, stop: Optional[List[str]] = None,
                 run_manager: Optional[CallbackManagerForLLMRun] = None,
@@ -32,6 +32,9 @@ class InternLM_LLM(LLM):
     
 if __name__ == "main":
     # 测试代码
-    llm = InternLM_LLM(model_path = "/root/data/model/Shanghai_AI_Laboratory/internlm-chat-7b")
+    llm = InternLM_LLM(model_path = "internlm/internlm2-chat-7b")
     print(llm.predict("你是谁"))
+
+
+
 
