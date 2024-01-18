332e400e65947c91873e82b8a558bf7841480d78
yestolife <zgjxncytl@gmail.com>
2024-01-18 20:14:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
-# 导入必要的库
 import gradio as gr
+# from langchain.vectorstores import Chroma
+# from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 import os
 from LLM import InternLM_LLM
+from langchain.prompts import PromptTemplate
 
 
 def load_chain():
     # 加载问答链
     # 定义 Embeddings
-    embeddings = HuggingFaceEmbeddings(model_name="/root/data/model/sentence-transformer")
+    # embeddings = HuggingFaceEmbeddings(model_name="/root/data/model/sentence-transformer")
 
-    # 向量数据库持久化路径
-    persist_directory = 'data_base/vector_db/chroma'
+    # # 向量数据库持久化路径
+    # persist_directory = 'data_base/vector_db/chroma'
 
-    # 加载数据库
-    vectordb = Chroma(
-        persist_directory=persist_directory,  # 允许我们将persist_directory目录保存到磁盘上
-        embedding_function=embeddings
-    )
+    # # 加载数据库
+    # vectordb = Chroma(
+    #     persist_directory=persist_directory,  # 允许我们将persist_directory目录保存到磁盘上
+    #     embedding_function=embeddings
+    # )
 
-    llm = InternLM_LLM(model_path = "/root/data/model/Shanghai_AI_Laboratory/internlm-chat-7b")
+    llm = InternLM_LLM(model_path = "internlm/internlm2-chat-7b")
 
     template = """使用以下上下文来回答最后的问题。如果你不知道答案，就说你不知道，不要试图编造答
     案。尽量使答案简明扼要。总是在回答的最后说“谢谢你的提问！”。
@@ -33,7 +35,7 @@ def load_chain():
     from langchain.chains import RetrievalQA
 
     qa_chain = RetrievalQA.from_chain_type(llm,
-                                        retriever=vectordb.as_retriever(),
+                                        retriever=None,
                                         return_source_documents=True,
                                         chain_type_kwargs={"prompt":QA_CHAIN_PROMPT})
     
@@ -60,7 +62,7 @@ class Model_center():
             return e, chat_history
 
 
-# model_center = Model_center()
+model_center = Model_center()
 
 block = gr.Blocks()
 with block as demo:
@@ -86,8 +88,8 @@ with block as demo:
                     components=[chatbot], value="Clear console")
                 
         # 设置按钮的点击事件。当点击时，调用上面定义的 qa_chain_self_answer 函数，并传入用户的消息和聊天历史记录，然后更新文本框和聊天机器人组件。
-        # db_wo_his_btn.click(model_center.qa_chain_self_answer, inputs=[
-        #                     msg, chatbot], outputs=[msg, chatbot])
+        db_wo_his_btn.click(model_center.qa_chain_self_answer, inputs=[
+                            msg, chatbot], outputs=[msg, chatbot])
         
     gr.Markdown("""提醒：<br>
     1. 初始化数据库时间可能较长，请耐心等待。
